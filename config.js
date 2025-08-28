2ecbf514c485e6f002464b9772cfc7f8f91611e7
Mo. Hajjar <mohajjar31@gmail.com>
2025-08-28 22:55:58 +0300

    Allow config.js for Netlify deployment

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAjKL8-QPcOKIXCC9L3K9EVRy2LfAcEhxI",
+  authDomain: "sweet-africa-bookings.firebaseapp.com",
+  projectId: "sweet-africa-bookings",
+  storageBucket: "sweet-africa-bookings.appspot.com",
+  messagingSenderId: "950167391030",
+  appId: "1:950167391030:web:1085602775ce912d220197",
+};
